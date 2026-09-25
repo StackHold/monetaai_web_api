7264ceade05e17461929eae5f14b43c643f99dd3
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-09-25 19:47:13 -0300

    feat: rota de listagem de todas as reunioes

--- src/main/java/br/com/MonetaAI/MonetaAI/controller/ReuniaoController.java
+++ src/main/java/br/com/MonetaAI/MonetaAI/controller/ReuniaoController.java
@@ -1,10 +1,28 @@
 package br.com.MonetaAI.MonetaAI.controller;
 
+import br.com.MonetaAI.MonetaAI.model.dao.ReuniaoDAO;
+import br.com.MonetaAI.MonetaAI.model.dto.ReuniaoDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class ReuniaoController {
     //TODO rota get listar todos
+
+    private final ReuniaoDAO reuniaoDAO;
+
+    ReuniaoController(ReuniaoDAO reuniaoDAO){
+        this.reuniaoDAO = reuniaoDAO;
+    }
+
+
+    @GetMapping("/reunioes")
+    public List<ReuniaoDto> getTodasReunioes(){
+        return reuniaoDAO.getTodasReunioes();
+    }
+
 
     //TODO rota post cadastrar nova reuniao
 }
