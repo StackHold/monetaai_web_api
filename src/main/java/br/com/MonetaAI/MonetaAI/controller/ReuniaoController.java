a4381302ace4e8bc32026dab35f947103e382435
Paulo Vieira <paulohenriqueavieira@gmail.com>
2026-09-25 19:34:30 -0300

    def chores

--- src/main/java/br/com/MonetaAI/MonetaAI/controller/ReuniaoController.java
+++ src/main/java/br/com/MonetaAI/MonetaAI/controller/ReuniaoController.java
@@ -4,4 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReuniaoController {
+    //TODO rota get listar todos
+
+    //TODO rota post cadastrar nova reuniao
 }
